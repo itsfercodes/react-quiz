d7ccc3aeb066221eaa6604461225ba71e1f82cc7
Fernando <itsfercodes@outlook.com>
2024-03-05 21:15:19 -0600

    Add finish screen and highscore functionality

---
+++ src/components/FinishScreen.js
@@ -0,0 +1,21 @@
+function FinishScreen({ points, maxPoints, highscore }) {
+  const percentage = (points / maxPoints) * 100;
+
+  let emoji;
+  if (percentage === 100) emoji = "🥇";
+  if (percentage >= 80 && percentage < 100) emoji = "🥳";
+  if (percentage >= 50 && percentage < 80) emoji = "🙃";
+  if (percentage >= 1 && percentage < 50) emoji = "🤨";
+  if (percentage === 0) emoji = "🤦";
+
+  return (
+    <>
+      <p className="result">
+        <span>{emoji}</span>You scored <strong>{points}</strong> out of{" "}
+        {maxPoints} ({Math.ceil(percentage)}%)
+      </p>
+      <p className="highscore">(Highscore: {highscore} points)</p>
+    </>
+  );
+}
+export default FinishScreen;
--- src/components/NextButton.js
+++ src/components/NextButton.js
@@ -1,12 +1,22 @@
-function NextButton({ dispatch, answer }) {
+function NextButton({ dispatch, answer, index, numQuestions }) {
   if (answer === null) return null;
 
+  // If there are questions left
+  if (index < numQuestions - 1) {
+    return (
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "nextQuestion" })}
+      >
+        Next
+      </button>
+    );
+  }
+
+  // If there are not questions left
   return (
-    <button
-      className="btn btn-ui"
-      onClick={() => dispatch({ type: "nextQuestion" })}
-    >
-      Next
+    <button className="btn btn-ui" onClick={() => dispatch({ type: "finish" })}>
+      Finish
     </button>
   );
 }
